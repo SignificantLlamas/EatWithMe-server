cc40d6cdbcf201e8e053aa19d6cf0478edf1aec2
Oliver <oliverbhuang@gmail.com>
2016-03-11 11:50:54 -0800

    Added comment on server srtart

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ var express = require('express');
 var db = require('./config/db.js')
 
 var app = express();
-app.listen(8000);
+app.listen(8000, function () {
+  console.log('Listening on port 8080...');
+});
 
 module.exports = app;
