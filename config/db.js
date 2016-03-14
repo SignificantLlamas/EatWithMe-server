6df5e96d6883cb7b151e84f0fe628d5b2d744e42
Oliver <oliverbhuang@gmail.com>
2016-03-14 11:17:33 -0700

    added bluebird promises to mongoose

--- config/db.js
+++ config/db.js
@@ -1,2 +1,3 @@
-var mongoose = require('mongoose');
+var Promise = require('bluebird');
+var mongoose = Promise.promisifyAll(require('mongoose'));
 module.exports = mongoose.connect('mongodb://localhost/family-style');
