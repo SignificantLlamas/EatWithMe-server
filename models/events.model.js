8d436742fbdc05e39bb01eea3fd2d33fe567fb66
joeycchin <joeycchin@gmail.com>
2016-03-14 11:51:26 -0700

    Events Schema

---
+++ models/events.model.js
@@ -0,0 +1,34 @@
+var mongoose = require('../config/db');
+
+var eventsSchema = new mongoose.Schema({
+  yelp_id: {
+    type: String,
+    required: true
+  },
+  dateTime: {
+    type: Date,
+    required: true
+  },
+  min: {
+    type: Number,
+    required: true
+  },
+  max: {
+    type: Number,
+    required: true
+  },
+  num_of_people: {
+    type: Number
+  },
+  restaurant_name: {
+    type: String,
+    required: true
+  },
+  restaurant_address: {
+    type: String,
+    required: true
+  },
+  users: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Users' }]
+});
+
+module.exports = mongoose.model('Events', eventsSchema);
