8352db4e09e9d89c594340424e7b29c43e341d43
Oliver <oliverbhuang@gmail.com>
2016-03-14 12:22:52 -0700

    commented out route temporarily

--- server.js
+++ server.js
@@ -4,7 +4,8 @@ var app = express();
 var db = require('./config/db.js');
 db.connect('mongodb://localhost/family-style');
 
-require('./config/routes.js')(app);
+// Uncomment after writing route
+// require('./config/routes.js')(app);
 
 app.listen(8000, function () {
   console.log('Listening on port 8080...');
