9b6edb977792aac06d38dbd295e8ba012fe22c28
Kathy Ems <redsangria@gmail.com>
2016-03-15 11:48:10 -0700

    Allows POST request with fields except list of users

--- controllers/events.controller.js
+++ controllers/events.controller.js
@@ -25,3 +25,23 @@ exports.getAll = function (req, res) {
     res.sendStatus(400).json(error);
   });
 };
+
+exports.create = function (req, res) {
+  Events.create({
+    yelp_id: req.body.yelpId,
+    dateTime: req.body.dateTime,
+    min: req.body.min,
+    max: req.body.max,
+    restaurant_name: req.body.restaurantName,
+    restaurant_address: req.body.restaurantAddress,
+    num_of_people: 1,
+    creatorId: req.body.userId,
+    users: [req.body.userId]
+  })
+  .then(function () {
+    res.sendStatus(201);
+  })
+  .catch(function (err) {
+    res.json(404, err);
+  });
+};
