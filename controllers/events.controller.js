37d7147a4bb8e1aa17d3ed22399721271662fa26
Oliver <oliverbhuang@gmail.com>
2016-03-14 20:36:54 -0700

    added getAll to events controller

--- controllers/events.controller.js
+++ controllers/events.controller.js
@@ -1,19 +1,27 @@
 var Events = require('../models/events.model');
 
-module.exports = {
-  getOne: function (req, res) {
-    var eventid = req.params.eventid;
+exports.getOne = function (req, res) {
+  var eventid = req.params.eventid;
 
-    Events.findOne({ yelp_id: eventid })
-    .then(function (foundEvent) {
-      if (foundEvent) {
-        res.status(201).json(foundEvent);
-      } else {
-        res.sendStatus(404);
-      }
-    })
-    .catch(function (error) {
-      console.log(error);
-    });
-  }
+  Events.findOne({ yelp_id: eventid })
+  .then(function (foundEvent) {
+    if (foundEvent) {
+      res.status(201).json(foundEvent);
+    } else {
+      res.sendStatus(404);
+    }
+  })
+  .catch(function (error) {
+    console.log(error);
+  });
+};
+
+exports.getAll = function (req, res) {
+  Events.find({ yelp_id: req.query.yelp_id })
+  .then(function (events) {
+    res.status(200).json(events);
+  })
+  .catch(function (error) {
+    res.sendStatus(400).json(error);
+  });
 };
