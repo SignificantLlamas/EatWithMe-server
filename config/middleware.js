c311c09101b679b1375904c28ee1189e61b91c96
joeycchin <joeycchin@gmail.com>
2016-03-16 10:31:39 -0700

    adding morgan

--- config/middleware.js
+++ config/middleware.js
@@ -1,8 +1,10 @@
 var bodyParser = require('body-parser');
 var cors = require('cors');
+var morgan = require('morgan');
 
 module.exports = function (app) {
   app.use(bodyParser.urlencoded({ extended: true }));
   app.use(bodyParser.json());
   app.use(cors());
+  app.use(morgan('dev'));
 };
