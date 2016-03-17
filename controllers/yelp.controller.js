f6078136566673727d26fc5a013b72f8030daa56
Oliver <oliverbhuang@gmail.com>
2016-03-17 16:01:03 -0700

    refactored yelp api controller

--- controllers/yelp.controller.js
+++ controllers/yelp.controller.js
@@ -1,46 +1,23 @@
-var oauthSignature = require('oauth-signature');
-var n = require('nonce')();
-var _ = require('underscore');
-var qs = require('querystring');
-var rp = require('request-promise');
+var Yelp = require('yelp');
 var keys = require('../config/keys.js');
+var _ = require('underscore');
 
-var url = 'http://api.yelp.com/v2/search';
-var httpMethod = 'GET';
+var yelp = new Yelp({
+  consumer_key: keys.yelp.consumerKey,
+  consumer_secret: keys.yelp.consumerSecret,
+  token: keys.yelp.token,
+  token_secret: keys.yelp.tokenSecret
+});
 
-var defaultTerm = ' food open now';
-var defaultParameters = {
-  sort: '2',
-  radius_filter: 500,
-  limit: 10
-};
-
-var requiredParameters = {
-  oauth_consumer_key: keys.yelp.consumerKey,
-  oauth_token: keys.yelp.token,
-  oauth_signature_method: 'HMAC-SHA1',
-  oauth_timestamp: n().toString().substr(0, 10),
-  oauth_nonce: n(),
-  oauth_version: '1.0'
-};
-
-var buildUrl = function (setParameters) {
-  var parameters = _.assign(defaultParameters, setParameters, requiredParameters);
-  parameters.term = (parameters.term || '') + defaultTerm;
-  parameters.oauth_signature = oauthSignature.generate(
-    httpMethod,
-    url,
-    parameters,
-    keys.yelp.consumerSecret,
-    keys.yelp.tokenSecret,
-    { encodeSignature: false }
-  );
-  return url + '?' + qs.stringify(parameters);
+var defaultSearch = {
+  category_filter: 'restaurants',
+  limit: 5
 };
 
 exports.search = function (req, res) {
-  var apiUrl = buildUrl(req.query);
-  rp(apiUrl)
+  var searchParams = _.assign(defaultSearch, req.query);
+
+  yelp.search(searchParams)
     .then(function (result) {
       res.status(200).json(result);
     })
