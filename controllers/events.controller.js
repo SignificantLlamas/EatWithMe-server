8d87b0e1e3f336265bdb8904a48fb2fd6e7179a3
joeycchin <joeycchin@gmail.com>
2016-03-17 15:06:53 -0700

    promiseAll for update

--- controllers/events.controller.js
+++ controllers/events.controller.js
@@ -1,6 +1,6 @@
 var Events = require('../models/events.model');
 var Users = require('../models/users.model');
-
+var Promise = require('bluebird');
 // gets all data for one event (restaurant)
 exports.getOne = function (req, res) {
   var eventId = req.params.eventId;
@@ -54,18 +54,22 @@ exports.update = function (req, res) {
   var eventId = req.params.eventId;
   var userId = req.body.userId;
 
-  Events.findByIdAndUpdate(eventId, { $addToSet: { users: userId } })
-  .then(function updateUser(foundEvent) {
-    if (!foundEvent) {
+  Promise.all(Events.findById(eventId), Users.findById(userId))
+  .then(function updateEventAndUser(findResults) {
+    if (!findResults[0]) {
       throw new Error('event not found');
     }
-    return Users.findByIdAndUpdate(userId, { $addToSet: { events: eventId } });
-  })
-  .then(function sendUpdateStatus(foundUser) {
-    if (!foundUser) {
-      throw new Error('users not found');
+
+    if (!findResults[1]) {
+      throw new Error('user not found');
     }
-    return res.sendStatus(202);
+
+    return Promise.all([Events.findByIdAndUpdate(eventId, { $addToSet: { users: userId } }),
+      Users.findByIdAndUpdate(userId, { $addToSet: { events: eventId } })]
+    );
+  })
+  .then(function sendStatus() {
+    res.sendStatus(202);
   })
   .catch(function updateError(error) {
     res.status(404).json({ error: error.message });
