188c8d666506c997b1e2969ba62f4374e100dcdd
Kathy Ems <redsangria@gmail.com>
2016-03-18 17:32:47 -0700

    sets up sockets foundation for server

--- server.js
+++ server.js
@@ -1,14 +1,24 @@
 var express = require('express');
 var app = express();
-
 var db = require('./config/db.js');
+var server = require('http').createServer(app);
+var io = require('socket.io')(server);
+
+var PORT = process.env.port || 8080;
+
 db.connect('mongodb://localhost/family-style');
 
 require('./config/middleware.js')(app);
 require('./config/routes.js')(app);
 
-app.listen(8080, function () {
-  console.log('Listening on port 8080...');
+server.listen(PORT, function () {
+  console.log('Listening on port ' + PORT);
+});
+
+io.on('connection', function ioConnect(socket) {
+  socket.on('test', function ioEmit() {
+    io.emit('hello World');
+  });
 });
 
 module.exports = app;
