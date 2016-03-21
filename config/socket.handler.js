23182763cee2dee31fc99cc684103670e0c7461b
Oliver <oliverbhuang@gmail.com>
2016-03-21 14:33:12 -0700

    set up backend for sockets

--- config/socket.handler.js
+++ config/socket.handler.js
@@ -1,14 +1,15 @@
 module.exports = function (io) {
   // each time this namespace is called, a new socket-room is created
   var tableio = io.of('/table');
+
   tableio.on('connection', function ioConnect(socket) {
-    socket.on('join', function ioEmit(eventId) {
-      console.log('socket join', eventId);
-      socket.join(eventId);
+    socket.on('join', function ioEmit(message) {
+      socket.join(message.eventId);
+      tableio.to(message.eventId).emit('updateUsers', message.users);
     });
+
     socket.on('emitMessage', function emitMessage(eventId, message) {
-      console.log('emit message', eventId, message);
-      tableio.to(eventId).emit('eventjoined', message);
+      tableio.to(eventId).emit('returnMessage', message);
     });
   });
 };
