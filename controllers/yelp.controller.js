9beb09bf5a47eec20a355a5bc8ac9477b8dd66e6
Kathy Ems <redsangria@gmail.com>
2016-03-24 17:29:20 -0700

    updates controller to use process.env variables for circleci integration

--- controllers/yelp.controller.js
+++ controllers/yelp.controller.js
@@ -2,10 +2,10 @@ var Yelp = require('yelp');
 var keys = require('../config/keys.js');
 
 var yelp = new Yelp({
-  consumer_key: keys.yelp.consumerKey,
-  consumer_secret: keys.yelp.consumerSecret,
-  token: keys.yelp.token,
-  token_secret: keys.yelp.tokenSecret
+  consumer_key: process.env.consumerKey || keys.yelp.consumerKey,
+  consumer_secret: process.env.consumerSecret || keys.yelp.consumerSecret,
+  token: process.env.token || keys.yelp.token,
+  token_secret: process.env.tokenSecret || keys.yelp.tokenSecret
 });
 
 exports.search = function search(searchParams) {
